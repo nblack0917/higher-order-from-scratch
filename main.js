bff743e380ec891d18e0b77ec87aaa4c29b5f24a
nblack0917 <nickablack@gmail.com>
2020-11-11 11:36:45 -0600

    reduce and filter from scratch

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let array = [1, 2, 3, 4, 5, 6, 12]
+let array = [1, 2, 3, 6, 4, 5, 6, 12]
 
 const mapFromScratch = (arr, func) => {
   let newArr = [];
@@ -6,6 +6,7 @@ const mapFromScratch = (arr, func) => {
     newArr.push(func(elem))
   }
   console.log("mapFromScratch: " + newArr)
+  return newArr
 }
 
 const addTwo = (num) => {
@@ -18,3 +19,25 @@ const addTen = (num) => {
 mapFromScratch(array, addTwo)
 mapFromScratch(array, addTen)
 
+const addArray = (arr => {
+  let value = 0;
+  for(elem of arr) {
+    value += elem
+  }
+  console.log("Total sum of array: " + value)
+})
+
+addArray(array)
+addArray(mapFromScratch(array, addTen))
+
+const filterFromScratch = (arr, searchTerm) => {
+  let newArr = []
+  for(elem of arr) {
+    if (elem == searchTerm) {
+      newArr.push(elem)
+    }
+  }
+  console.log("Found results: " + newArr)
+}
+
+filterFromScratch(array, 6)
